cda5bff2a49fcdb1c05e07152e8f433c414ca5a0
Harish Nair <nairharish1999@gmail.com>
2023-12-01 02:32:57 -0500

    added cude:0 gpu device training

--- audio_generator_elaborated.py
+++ audio_generator_elaborated.py
@@ -1,4 +1,7 @@
 from transformers import AutoProcessor, AutoModel
+import torch
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 processor = AutoProcessor.from_pretrained("suno/bark")
 model = AutoModel.from_pretrained("suno/bark")
@@ -6,11 +9,11 @@ model = AutoModel.from_pretrained("suno/bark")
 voice_preset = "v2/en_speaker_6"
 
 inputs = processor(
-    text=["Hello, my name is Harish. They call me Doniker, uh — and I like Red Dead Redemption 2. [laughs] But I also like other games such as GTA 5."],
+    text=["Hello, my name is Harish. They call me Doniker, uh — and I like Red Dead Redemption 2. [laughs] But I also like other games such as GTA 5 AAAAAAAAAAAAA Kaiose pro max"],
     return_tensors="pt", voice_preset=voice_preset
 )
 
-speech_values = model.generate(**inputs, do_sample=True)
+speech_values = model.generate(**inputs, do_sample=True).to(device)
 
 import scipy
 
