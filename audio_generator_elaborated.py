e4cf202dbc5a4baf14cc10c367620f520e567a0b
Harish Nair <nairharish1999@gmail.com>
2024-06-09 06:30:56 -0400

    fix: gpu dump bug

--- audio_generator_elaborated.py
+++ audio_generator_elaborated.py
@@ -2,20 +2,21 @@ from transformers import AutoProcessor, AutoModel
 import torch
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
+print(device)
 
 processor = AutoProcessor.from_pretrained("suno/bark")
-model = AutoModel.from_pretrained("suno/bark")
+model = AutoModel.from_pretrained("suno/bark").to(device)
 
 voice_preset = "v2/en_speaker_6"
 
 inputs = processor(
-    text=["Hello, my name is Harish. They call me Doniker, uh — and I like Red Dead Redemption 2. [laughs] But I also like other games such as GTA 5 AAAAAAAAAAAAA Kaiose pro max"],
+    text=["Hello, my name is Harish Nair and I need assistance with my course selection and co-op work term!"],
     return_tensors="pt", voice_preset=voice_preset
-)
+).to(device)
 
 speech_values = model.generate(**inputs, do_sample=True).to(device)
 
-import scipy
+import scipy  # noqa: E402
 
 sampling_rate = model.generation_config.sample_rate
-scipy.io.wavfile.write("bark_out.wav", rate=sampling_rate, data=speech_values.cpu().numpy().squeeze())
+scipy.io.wavfile.write("mdti.wav", rate=sampling_rate, data=speech_values.cpu().numpy().squeeze())
